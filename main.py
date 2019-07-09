80ecd366939ff4fc5ea41bc700aa034df557de81
David <yonghoon95@gmail.com>
2019-07-09 13:45:31 +0900

    Complete!
    Gradient Descent 구현 완료. 구조와 원리를 이해하는데에 확실히 도움이 되었음.

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def calculateCost(X, Y, weight, bias):
 	return ((weight * X + bias - Y) ** 2).sum() / len(X)
 
 
-def numericalDifferentiation(f, x, y, weight, bias):
-	deltaX = 1e-7
-	upCost = f(x + deltaX, y, weight, bias)
-	diff = (upCost - f(x - deltaX, y, weight, bias)) / (deltaX * 2)
+def numericalDifferentiationWeight(f, x, y, weight, bias):
+	deltaW = 1e-7
+	upCost = f(x, y, weight + deltaW, bias)
+	diff = (upCost - f(x, y, weight - deltaW, bias)) / (deltaW* 2)
+	return diff, upCost
 
+def numericalDifferentiationBias(f, x, y, weight, bias):
+	deltaB = 1e-7
+	upCost = f(x, y, weight, bias + deltaB)
+	diff = (upCost - f(x, y, weight, bias - deltaB)) / (deltaB * 2)
 	return diff, upCost
 
 
@@ -19,15 +24,14 @@ def gradientDescent(X, Y, weight, bias, learningRate, epoch):
 	cost_list = []
 
 	for i in range(epoch):
-		diff = numericalDifferentiation(calculateCost, X, Y, weight, bias)
-		if diff[0] < 0:
-			weight = weight - learningRate * diff[0]
+		diffW = numericalDifferentiationWeight(calculateCost, X, Y, weight, bias)
+		weight = weight - learningRate * diffW[0]
+		diffB = numericalDifferentiationBias(calculateCost, X, Y, weight, bias)
+		bias = bias - learningRate * diffB[0]
 
-		else:
-			weight = weight + learningRate * diff[0]
+		diff_list.append(diffB[0])
+		cost_list.append(diffB[1])
 
-		diff_list.append(diff[0])
-		cost_list.append(diff[1])
 	return weight, diff_list, cost_list
 
 X = 2 * np.random.rand(100, 1)
@@ -35,7 +39,7 @@ Y = 8 + 5 * X + np.random.randn(100, 1)
 w = np.random.randn(2, 1)
 
 learningRate = 0.01
-epoch = 10000
+epoch = 1000
 
 result = gradientDescent(X, Y, w[0], w[1], learningRate, epoch)
 
